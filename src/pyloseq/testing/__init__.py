76d6999f5af41b35a38f3d611bcec11df7c47b6a
Mike Donovan <michael.donovan.ext@wisdompanel.com>
2026-05-30 09:37:51 -0400

    fixture tests

---
+++ src/pyloseq/testing/__init__.py
@@ -0,0 +1,15 @@
+from pyloseq.testing.fixtures import (
+    load_enterotype_reference,
+    load_esophagus_reference,
+    load_global_patterns_reference,
+    load_golden,
+    load_soilrep_reference,
+)
+
+__all__ = [
+    "load_global_patterns_reference",
+    "load_enterotype_reference",
+    "load_esophagus_reference",
+    "load_soilrep_reference",
+    "load_golden",
+]
